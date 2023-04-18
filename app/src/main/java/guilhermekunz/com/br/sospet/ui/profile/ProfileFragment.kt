fba0d7cb52f82efc72af719d448801dfa499b139
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2023-04-18 17:57:57 -0300

    Feat: WIP Profile Fragment

--- app/src/main/java/guilhermekunz/com/br/sospet/ui/profile/ProfileFragment.kt
+++ app/src/main/java/guilhermekunz/com/br/sospet/ui/profile/ProfileFragment.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import guilhermekunz.com.br.sospet.R
+import guilhermekunz.com.br.sospet.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+
 
 }

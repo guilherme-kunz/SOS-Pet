a60d6a02c52a21bad66e989f26273cc0a0b43177
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2023-05-03 16:48:12 -0300

    Feat: Splash Screen

---
+++ app/src/main/java/guilhermekunz/com/br/sospet/ui/form/FormFragment.kt
@@ -0,0 +1,38 @@
+package guilhermekunz.com.br.sospet.ui.form
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import guilhermekunz.com.br.sospet.databinding.FragmentFormBinding
+
+
+class FormFragment : Fragment() {
+
+    private var _binding: FragmentFormBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFormBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    //Imagem
+    //Especie
+    //Raça
+    //Cor da Pele
+    //Porte
+    //Localização
+
+
+
+}
